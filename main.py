377eb4b3a4688317c2f10c4785b87d34c830ea74
@ez85asyh <maryam.rani@fau.de>
2026-06-01 01:47:24 +0200

    add station search from iRail API

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ from datetime import datetime
 
 app = FastAPI()
 @app.get("/departures")
-def get_dep(q:str = Query(...)):
+async def get_dep(q:str = Query(...)):
     if len(q) < 3:
       raise HTTPException(
         status_code=400,
         detail = "Query must be at least 3 characters long"
       )
-    return {"message": "working", "query": q}
+    # return {"message": "working", "query": q}
+    async with httpx.AsyncClient(timeout=10.0,follow_redirects=True) as client:
+       response = await client.get("https://api.irail.be/stations/?format=json&lang=en",
+                                   headers={"User-Agent": "lagovia-train-tracker/1.0"})
+       all_data = response.json()
+       stations = all_data["station"]
+
+    #    return  {"status": response.status_code, "text": response.text[:500]}
+
+    match_station = [
+       s for s in stations
+       if q.lower() in s["name"].lower()
+    ]
+
+    return {"query": q, "matched_station_found" : len(match_station), "stations" : match_station}
 
 
 
