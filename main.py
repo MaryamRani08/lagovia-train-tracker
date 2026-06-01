fb4f4d64a590b91555630ecf18ce21c9f4fa86fc
@ez85asyh <maryam.rani@fau.de>
2026-06-01 13:09:16 +0200

    add error handling for iRail API

--- main.py
+++ main.py
@@ -11,54 +11,68 @@ async def get_dep(q:str = Query(...)):
         detail = "Query must be at least 3 characters long"
       )
     # return {"message": "working", "query": q}
-    async with httpx.AsyncClient(timeout=10.0,follow_redirects=True) as client:
-       response = await client.get("https://api.irail.be/stations/?format=json&lang=en",
+    try:
+       async with httpx.AsyncClient(timeout=10.0,follow_redirects=True) as client:
+          response = await client.get("https://api.irail.be/stations/?format=json&lang=en",
                                    headers={"User-Agent": "lagovia-train-tracker/1.0"})
-       all_data = response.json()
-       stations = all_data["station"]
+          all_stations = response.json()["station"]
+        #   stations = all_data["station"]
 
     #    return  {"status": response.status_code, "text": response.text[:500]}
-
+    except Exception: 
+          raise HTTPException(
+             status_code= 503,
+             detail="could not reach iRail API. Try again later."
+          )     
+         
     match_stations = [
-       s for s in stations
-       if q.lower() in s["name"].lower()
-    ]
+             s for s in all_stations
+             if q.lower() in s["name"].lower()
+                 ]
 
     # return {"query": q, "matched_station_found" : len(match_station), "stations" : match_station}
     
     results = []
     
     for station in match_stations:
-       async with httpx.AsyncClient(timeout=10.0,follow_redirects=True) as client:
-          lb_response = await client.get(
-             "https://api.irail.be/liveboard/",
-             params={"station": station["name"], "format": "json", "lang": "en"},
-             headers= { "User-Agent": "lagovia-train-tracker/1.0"}
-          )
+       try:
+          async with httpx.AsyncClient(timeout=10.0,follow_redirects=True) as client:
+             lb_response = await client.get(
+                "https://api.irail.be/liveboard/",
+                params={"station": station["name"], "format": "json", "lang": "en"},
+                headers= { "User-Agent": "lagovia-train-tracker/1.0"}
+                )
+             
+             lb_data = lb_response.json()
+             
+             departures_all = lb_data.get("departures", {}).get("departure",[])
 
-          lb_data = lb_response.json()
+             now_time = datetime.now(timezone.utc)
+             selected_time = now_time + timedelta(minutes=15) 
 
-          departures_all = lb_data.get("departures", {}).get("departure",[])
+             filtered_dep = []
 
-          now_time = datetime.now(timezone.utc)
-          selected_time = now_time + timedelta(minutes=15) 
-
-          filtered_dep = []
-
-          for depar in departures_all:
-             dep_time = datetime.fromtimestamp(int(depar["time"]), tz=timezone.utc)
-             if now_time <=dep_time <= selected_time:
-                filtered_dep.append({
-                   "train" : depar["vehicle"],
-                   "destination" : depar["station"],
-                   "scheduled_time" : dep_time.strftime("%H:%M"),
-                   "delay_minutes" : int(depar["delay"]) // 60 
-                })
-
+             for depar in departures_all:
+                dep_time = datetime.fromtimestamp(int(depar["time"]), tz=timezone.utc)
+                if now_time <=dep_time <= selected_time:
+                   filtered_dep.append({
+                      "train" : depar["vehicle"],
+                      "destination" : depar["station"],
+                      "scheduled_time" : dep_time.strftime("%H:%M"),
+                      "delay_minutes" : int(depar["delay"]) // 60 
+                        })
+                   
+                   results.append({
+                      "station" : station["name"],
+                      "departures" : filtered_dep
+                      
+                      })
+       except Exception:
           results.append({
              "station" : station["name"],
-             "departures" : filtered_dep
-        })
+             "departures" : [],
+             "error": "could not fetch departures for this station"
+             })
           
     return {"query" : q, "stations_found": len(match_stations), "results": results}
 
