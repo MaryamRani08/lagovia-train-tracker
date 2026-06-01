0a6384ee07814bece2221a9913fc3ff93bef7009
@ez85asyh <maryam.rani@fau.de>
2026-06-01 11:40:09 +0200

    add departure fetching within 15 mintues from mow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query, HTTPException
 import httpx
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 app = FastAPI()
 @app.get("/departures")
@@ -19,13 +19,50 @@ async def get_dep(q:str = Query(...)):
 
     #    return  {"status": response.status_code, "text": response.text[:500]}
 
-    match_station = [
+    match_stations = [
        s for s in stations
        if q.lower() in s["name"].lower()
     ]
 
-    return {"query": q, "matched_station_found" : len(match_station), "stations" : match_station}
+    # return {"query": q, "matched_station_found" : len(match_station), "stations" : match_station}
+    
+    results = []
+    
+    for station in match_stations:
+       async with httpx.AsyncClient(timeout=10.0,follow_redirects=True) as client:
+          lb_response = await client.get(
+             "https://api.irail.be/liveboard/",
+             params={"station": station["name"], "format": "json", "lang": "en"},
+             headers= { "User-Agent": "lagovia-train-tracker/1.0"}
+          )
 
+          lb_data = lb_response.json()
+
+          departures_all = lb_data.get("departures", {}).get("departure",[])
+
+          now_time = datetime.now(timezone.utc)
+          selected_time = now_time + timedelta(minutes=15) 
+
+          filtered_dep = []
+
+          for depar in departures_all:
+             dep_time = datetime.fromtimestamp(int(depar["time"]), tz=timezone.utc)
+             if now_time <=dep_time <= selected_time:
+                filtered_dep.append({
+                   "train" : depar["vehicle"],
+                   "destination" : depar["station"],
+                   "scheduled_time" : dep_time.strftime("%H:%M"),
+                   "delay_minutes" : int(depar["delay"]) // 60 
+                })
+
+          results.append({
+             "station" : station["name"],
+             "departures" : filtered_dep
+        })
+          
+    return {"query" : q, "stations_found": len(match_stations), "results": results}
+
+          
 
 
 
